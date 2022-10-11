9e4411547ea59d179f6e55bc8f7e2f1131002317
hakurouken <ticshot@gmail.com>
2022-10-11 10:28:27 +0800

    build: use esbuild instead of tsc

---
+++ scripts/build.mjs
@@ -0,0 +1,19 @@
+import * as path from 'node:path';
+import { readFile } from 'node:fs/promises';
+import * as esbuild from 'esbuild';
+
+function readJson(file) {
+  return readFile(file, 'utf-8').then(JSON.parse);
+}
+
+const packageJson = await readJson(path.join(__dirname, '..', 'package.json'));
+
+await esbuild.build({
+  bundle: true,
+  format: 'esm',
+  entryPoints: ['src/index.ts'],
+  outfile: 'dist/punch.mjs',
+  external: Object.keys(packageJson.dependencies || {}),
+  platform: 'node',
+  target: 'node14'
+});
--- src/bin/run.ts
+++
@@ -1,8 +0,0 @@
-import { runAll } from '@/runners.js';
-
-try {
-  runAll();
-} catch (e) {
-  console.error(e);
-  process.exit(1);
-}
