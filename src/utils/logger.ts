8a6799da2bb0c097cae36f642cf1b41e6573742e
hakurouken <ticshot@gmail.com>
2022-10-11 15:06:20 +0800

    feat: debug log

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -6,10 +6,27 @@ export function createLogger(
   options: { verbose?: boolean; logPath: string }
 ) {
   const { verbose = false, logPath } = options;
-  return pino(
-    { name, level: verbose ? 'debug' : 'info' },
-    pino.destination(path.join(logPath, `${name}.log`))
-  );
+  return pino({
+    transport: {
+      targets: [
+        {
+          level: verbose ? 'debug' : 'info',
+          target: 'pino/file',
+          options: { destination: path.join(logPath, `${name}.log`) }
+        },
+      ].concat(
+        verbose
+          ? [
+              {
+                level: 'debug',
+                target: 'pino-pretty',
+                options: { destination: 1 as any }
+              }
+            ]
+          : []
+      )
+    }
+  });
 }
 
 export type { Logger } from 'pino';
